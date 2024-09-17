ba3199b7fcc3a79937277e8bfdcf2dff1df87eac
INTEL <INTEL@DESKTOP-SUF5311>
2024-09-17 11:12:26 +0530

    java basic program

---
+++ src/com/typecasting/tycastingdata.java
@@ -0,0 +1,21 @@
+package com.typecasting;
+
+public class tycastingdata {
+	
+	 public static void main(String[] args) {
+		 double d=344.09;
+			int p=(int)d;
+			
+//			boolean z=true;
+//			int x=z;
+			
+			
+			
+			int k=123;
+			float f=k;
+			System.out.println(f);
+			System.out.println(p);
+	}
+	
+
+}
---
+++ src/firstproject/primeno.java
@@ -0,0 +1,17 @@
+package firstproject;
+
+public class primeno {
+
+	public static void main(String[] args) {
+
+	int count=0;
+	for(int i=10;i<=20;i++)
+	{
+		if(i%5==0)
+		{
+			count++;
+		}
+		System.out.print(count);
+	}
+}
+}
